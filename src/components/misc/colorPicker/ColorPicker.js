cad3d7ebea4b1a3257459f839338d8c0fc5a04e3
alexsaliu1 <alex@madeinkatana.com>
2020-11-01 23:22:29 +1030

    Color picker color slider now changes base color, color lightness selector returns correct hex value

--- src/components/misc/colorPicker/ColorPicker.js
+++ src/components/misc/colorPicker/ColorPicker.js
@@ -4,43 +4,48 @@ import './colorPicker.css';
 
 const ColorPicker = () => {
     const [color, setColor] = useState('#FF0000');
-    const [backgroundRgb, setBackgroundRgb] = useState([255, 0, 0]);
+    const [baseRgb, setBaseRgb] = useState([255, 0, 0]);
     const [selectorPosition, setSelectorPosition] = useState([20, 20]);
     const [colorSliderValue, setColorSliderValue] = useState(0);
+
+    useEffect(() => {
+        console.log(baseRgb);
+        
+    }, [baseRgb])
 
     useEffect(() => {
         // h 360 - 0
         // s 0 - 100% left to right
         // l 0 - 50% bottom to top | 0 - 50 right to left
 
-        let s = selectorPosition[0] / 260 * 100;
-        const ly = Math.round((100 - selectorPosition[0] / 260 * 100) / 2);
-        const lx = Math.round((100 - selectorPosition[1] / 104 * 100) / 2);
-        let l = ly + lx;
+        // let s = selectorPosition[0] / 260 * 100;
+        // const ly = Math.round((100 - selectorPosition[0] / 260 * 100) / 2);
+        // const lx = Math.round((100 - selectorPosition[1] / 104 * 100) / 2);
+        // let l = ly + lx;
 
-        const hex = hslToHex(360, s, l);
-        console.log(hex);
-        console.log("s: ", s);
-        console.log("l: ", l);
+        // const hex = hslToHex(360, s, l);
+        // console.log(hex);
+        // console.log("s: ", s);
+        // console.log("l: ", l);
 
-            // console.log("backgroundRgb: ", backgroundRgb);
-            // let newRgb = [0,0,0];
-            // for (let i = 0; i < 3; i++) {
-            //     const distanceFromRight = 260 - selectorPosition[0];
-            //     const percentageDecimalFromRight = distanceFromRight / 260;
-            //     const rgbRange = 255 - backgroundRgb[i];
-            //     const rgb = percentageDecimalFromRight * rgbRange + backgroundRgb[i];
-            //     newRgb[i] = Math.round(rgb);
-            // }
-            // for (let i = 0; i < 3; i++) {
-            //     const distanceFromTop = selectorPosition[1];
-            //     const percentageDecimalFromTop = distanceFromTop / 104;
-            //     const rgbRange = newRgb[i];
-            //     const rgb = newRgb[i] - percentageDecimalFromTop * rgbRange;
-            //     newRgb[i] = Math.round(rgb);
-            // }
-            // console.log("CONVERTED: ", convert(newRgb));
-            // setColor(convert(newRgb));
+            console.log("backgroundRgb: ", baseRgb);
+            let newRgb = [0,0,0];
+            for (let i = 0; i < 3; i++) {
+                const distanceFromRight = 260 - selectorPosition[0];
+                const percentageDecimalFromRight = distanceFromRight / 260;
+                const rgbRange = 255 - baseRgb[i];
+                const rgb = percentageDecimalFromRight * rgbRange + baseRgb[i];
+                newRgb[i] = Math.round(rgb);
+            }
+            for (let i = 0; i < 3; i++) {
+                const distanceFromTop = selectorPosition[1];
+                const percentageDecimalFromTop = distanceFromTop / 104;
+                const rgbRange = newRgb[i];
+                const rgb = newRgb[i] - percentageDecimalFromTop * rgbRange;
+                newRgb[i] = Math.round(rgb);
+            }
+            console.log("CONVERTED: ", convert(newRgb));
+            setColor(convert(newRgb));
 
     }, [selectorPosition])
 
@@ -67,34 +72,59 @@ const ColorPicker = () => {
         }
     }
 
-    const hslToHex = (h, s, l) => {
-        h /= 360;
-        s /= 100;
-        l /= 100;
-        let r, g, b;
-        if (s === 0) {
+    // const hslToHex = (h, s, l) => {
+    //     h /= 360;
+    //     s /= 100;
+    //     l /= 100;
+    //     let r, g, b;
+    //     if (s === 0) {
+    //         r = g = b = l; // achromatic
+    //     }
+    //     else {
+    //         const hue2rgb = (p, q, t) => {
+    //             if (t < 0) t += 1;
+    //             if (t > 1) t -= 1;
+    //             if (t < 1 / 6) return p + (q - p) * 6 * t;
+    //             if (t < 1 / 2) return q;
+    //             if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+    //             return p;
+    //         };
+    //         const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    //         const p = 2 * l - q;
+    //         r = hue2rgb(p, q, h + 1 / 3);
+    //         g = hue2rgb(p, q, h);
+    //         b = hue2rgb(p, q, h - 1 / 3);
+    //     }
+    //     const toHex = x => {
+    //     const hex = Math.round(x * 255).toString(16);
+    //     return hex.length === 1 ? '0' + hex : hex;
+    //     };
+    //     return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+    // }
+
+    function hslToRgb(h, s, l){
+        var r, g, b;
+    
+        if(s == 0){
             r = g = b = l; // achromatic
-        }
-        else {
-            const hue2rgb = (p, q, t) => {
-                if (t < 0) t += 1;
-                if (t > 1) t -= 1;
-                if (t < 1 / 6) return p + (q - p) * 6 * t;
-                if (t < 1 / 2) return q;
-                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        }else{
+            var hue2rgb = function hue2rgb(p, q, t){
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
                 return p;
-            };
-            const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            const p = 2 * l - q;
-            r = hue2rgb(p, q, h + 1 / 3);
+            }
+    
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
             g = hue2rgb(p, q, h);
-            b = hue2rgb(p, q, h - 1 / 3);
+            b = hue2rgb(p, q, h - 1/3);
         }
-        const toHex = x => {
-        const hex = Math.round(x * 255).toString(16);
-        return hex.length === 1 ? '0' + hex : hex;
-        };
-        return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+    
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
     }
 
     const moveColorSelector = (rect) => {
@@ -139,13 +169,13 @@ const ColorPicker = () => {
                     onMouseDown={(e) => handelColorSlider(e)}
                     className="containerTop"
                 >
-                    <div className="backgroundContainer" style={{background: `rgb(${backgroundRgb[0]}, ${backgroundRgb[1]}, ${backgroundRgb[2]})`}}></div>
+                    <div className="backgroundContainer" style={{background: `rgb(${baseRgb[0]}, ${baseRgb[1]}, ${baseRgb[2]})`}}></div>
                     <div className="backgroundWhite"></div>
                     <div className="backgroundBlack"></div>
                     <div className="colorSelector" style={{left: `${selectorPosition[0]}px` ,top: `${selectorPosition[1]}px`,}}></div>
                 </div>
                 <div className="colorSliderBar">
-                    <input onChange={(e) => setColorSliderValue(e.target.value)} value={colorSliderValue} className="inputSlider" type="range" min="0" max="255"/>
+                    <input onChange={(e) => {setBaseRgb(hslToRgb(e.target.value/360, 1, 0.5)); setColorSliderValue(e.target.value)}} value={colorSliderValue} className="inputSlider" type="range" min="0" max="360"/>
                 </div>
                 <input onChange={(e) => changeHex(e.target.value)} className="hexInput" value={color} type="text" />
             </div>
