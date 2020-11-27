c15e2a97e4a9f6000703306b49744b8b5a6932df
Alex <alex.saliu@gmail.com>
2020-11-27 22:45:42 +1030

    Completed color picker functionality - Updating slider, color selector, hex input

--- src/components/misc/colorPicker/ColorPicker.js
+++ src/components/misc/colorPicker/ColorPicker.js
@@ -1,20 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import convert from 'color-convert'
 
-import './colorPicker.css';
+import './colorPicker.css'
+
+const WIDTH = 260
+const HEIGHT = 104
 
 const ColorPicker = () => {
-    const [color, setColor] = useState('#FF0000');
-    const [baseRgb, setBaseRgb] = useState([255, 0, 0]);
-    const [selectorPosition, setSelectorPosition] = useState([20, 20]);
-    const [colorSliderValue, setColorSliderValue] = useState(0);
+    const [color, setColor] = useState('FF0000')
+    const [baseRgb, setBaseRgb] = useState([255, 0, 0])
+    const [selectorPosition, setSelectorPosition] = useState([WIDTH, 0])
+    const [colorSliderValue, setColorSliderValue] = useState(0)
 
     useEffect(() => {
-        console.log(baseRgb);
-        const newRgb = calculateNewRgb(baseRgb, selectorPosition[0], selectorPosition[1]);
-        setColor(convert(newRgb));
+        const newRgb = calculateNewRgb(baseRgb, selectorPosition[0], selectorPosition[1])
+        setColor(convert.rgb.hex(newRgb))
     }, [baseRgb, selectorPosition])
 
-    const calculateNewRgb = (baseRgb, xPosition, yPosition, width=260, height=104) => {
+    const calculateNewRgb = (baseRgb, xPosition, yPosition, width=WIDTH, height=HEIGHT) => {
         let newRgb = [0,0,0];
         for (let i = 0; i < 3; i++) {
             const distanceFromRight = width - xPosition;
@@ -34,142 +37,52 @@ const ColorPicker = () => {
     }
 
     const changeHex = (val) => {
-        if (!val) return;
-        setColor(val);
-        const rgb = convert(val);
-        const newHsl = rgbToHsl(...rgb);
-        setColorSliderValue(newHsl[0] * 360)
+        if (!val) return
+        setColor(val)
+        const hsl = convert.hex.hsl(val)
+        setColorSliderValue(hsl[0])
+        setBaseRgb(convert.hsl.rgb([hsl[0], 100, 50]))
+        updateSelectorPosition(convert.hsl.hsv(hsl))
     }
 
-    const convert = (val) => {
-        if (val[0] === "#") {
-            const rgb = [];
-            for (let i = 0; i < 6; i+=2) {
-                const n = parseInt(val[i+1] + val[i+2], 16);
-                rgb.push(n >= 0 ? n : 0);
-            }
-            return rgb
-        }
-        else {
-            let hex = "#";
-            for (let i = 0; i < 3; i++) {
-                let hexVal = val[i].toString(16);
-                hex += hexVal.length === 1 ? "0" + hexVal : hexVal;
-            }
-            return hex;
-        }
-    }
-
-    // const hslToHex = (h, s, l) => {
-    //     h /= 360;
-    //     s /= 100;
-    //     l /= 100;
-    //     let r, g, b;
-    //     if (s === 0) {
-    //         r = g = b = l; // achromatic
-    //     }
-    //     else {
-    //         const hue2rgb = (p, q, t) => {
-    //             if (t < 0) t += 1;
-    //             if (t > 1) t -= 1;
-    //             if (t < 1 / 6) return p + (q - p) * 6 * t;
-    //             if (t < 1 / 2) return q;
-    //             if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-    //             return p;
-    //         };
-    //         const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    //         const p = 2 * l - q;
-    //         r = hue2rgb(p, q, h + 1 / 3);
-    //         g = hue2rgb(p, q, h);
-    //         b = hue2rgb(p, q, h - 1 / 3);
-    //     }
-    //     const toHex = x => {
-    //     const hex = Math.round(x * 255).toString(16);
-    //     return hex.length === 1 ? '0' + hex : hex;
-    //     };
-    //     return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
-    // }
-
-    function hslToRgb(h, s, l){
-        var r, g, b;
-    
-        if(s == 0){
-            r = g = b = l; // achromatic
-        }else{
-            var hue2rgb = function hue2rgb(p, q, t){
-                if(t < 0) t += 1;
-                if(t > 1) t -= 1;
-                if(t < 1/6) return p + (q - p) * 6 * t;
-                if(t < 1/2) return q;
-                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-                return p;
-            }
-    
-            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            var p = 2 * l - q;
-            r = hue2rgb(p, q, h + 1/3);
-            g = hue2rgb(p, q, h);
-            b = hue2rgb(p, q, h - 1/3);
-        }
-    
-        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-    }
-
-    function rgbToHsl(r, g, b){
-        r /= 255
-        g /= 255
-        b /= 255
-        var max = Math.max(r, g, b), min = Math.min(r, g, b);
-        var h, s, l = (max + min) / 2;
-    
-        if(max == min){
-            h = s = 0; // achromatic
-        }else{
-            var d = max - min;
-            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-            switch(max){
-                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-                case g: h = (b - r) / d + 2; break;
-                case b: h = (r - g) / d + 4; break;
-            }
-            h /= 6;
-        }
-    
-        return [h, s, l];
+    const updateSelectorPosition = (hsv) => {
+        const xPosition = hsv[1] * (WIDTH / 100)
+        const yPosition = 100 - hsv[2] * (HEIGHT / 100)
+        setSelectorPosition([xPosition, yPosition])
     }
 
     const moveColorSelector = (rect) => {
         return (e) => {
             setSelectorPosition(position => {
-                let x = position[0] + e.movementX;
-                let y = position[1] + e.movementY;
+                let x = position[0] + e.movementX
+                let y = position[1] + e.movementY
                 if (e.clientY < rect.top + 4) y = 0
-                if (e.clientY > rect.top + rect.height - 8) y = 104
+                if (e.clientY > rect.top + rect.height - 8) y = HEIGHT
                 if (e.clientX < rect.left + 4) x = 0
-                if (e.clientX > rect.left + rect.width - 8) x = 260
+                if (e.clientX > rect.left + rect.width - 8) x = WIDTH
                 return [x, y]
             })
         }
     }
 
     const setInitialPosition = (e, rect) => {
-        let x = e.clientX - rect.left;
-        let y = e.clientY - rect.top;
-        x = x > 260 ? 260 : x;
-        y = y > 104 ? 104 : y;
-        return [x, y];
+        let x = e.clientX - rect.left
+        let y = e.clientY - rect.top
+        x = x > WIDTH ? WIDTH : x
+        y = y > HEIGHT ? HEIGHT : y
+        return [x, y]
     }
 
     const handelColorSlider = (e) => {
         const rect = document.querySelector('.containerTop').getBoundingClientRect();
-        setSelectorPosition(setInitialPosition(e, rect));
-        const windowMouseMove = moveColorSelector(rect);
-        document.querySelector('.containerTop').style.cursor = 'none';
-        window.addEventListener('mousemove', windowMouseMove);
+        setSelectorPosition(setInitialPosition(e, rect))
+        const windowMouseMove = moveColorSelector(rect)
+        document.querySelector('.containerTop').style.cursor = 'none'
+        window.addEventListener('mousemove', windowMouseMove)
         window.addEventListener('mouseup', function mouseup() {
-            document.querySelector('.containerTop').style.cursor = 'unset';
-            window.removeEventListener('mousemove', windowMouseMove);
-            window.removeEventListener('mouseup', mouseup);
+            document.querySelector('.containerTop').style.cursor = 'unset'
+            window.removeEventListener('mousemove', windowMouseMove)
+            window.removeEventListener('mouseup', mouseup)
         })
     }
 
@@ -186,7 +99,7 @@ const ColorPicker = () => {
                     <div className="colorSelector" style={{left: `${selectorPosition[0]}px` ,top: `${selectorPosition[1]}px`,}}></div>
                 </div>
                 <div className="colorSliderBar">
-                    <input onChange={(e) => {setBaseRgb(hslToRgb(e.target.value/360, 1, 0.5)); setColorSliderValue(e.target.value)}} value={colorSliderValue} className="inputSlider" type="range" min="0" max="360"/>
+                    <input onChange={(e) => {setBaseRgb(convert.hsl.rgb(parseInt(e.target.value), 100, 50)); setColorSliderValue(e.target.value)}} value={colorSliderValue} className="inputSlider" type="range" min="0" max="360"/>
                 </div>
                 <input onChange={(e) => changeHex(e.target.value)} className="hexInput" value={color} type="text" />
             </div>
@@ -194,4 +107,4 @@ const ColorPicker = () => {
     );
 }
 
-export default ColorPicker;
+export default ColorPicker
